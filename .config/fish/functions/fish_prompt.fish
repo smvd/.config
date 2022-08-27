a3bcc91d94a569c46b6fbb801c3b5f12e7b70c4d
smvd <semvd06@gmail.com>
2022-08-27 21:27:34 +0200

    Added spaces to fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-       printf "%s  %s@%s     %s %s\n  " (set_color -r) $USER $hostname (prompt_pwd -D 128) (set_color normal)
+       printf "%s  %s @ %s     %s %s\n  " (set_color -r) $USER $hostname (prompt_pwd -D 128) (set_color normal)
 end
