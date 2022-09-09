8fa8ed52d8421b272a1792c02c831e120b889374
smvd <semvd06@gmail.com>
2022-09-09 13:41:33 +0200

    I changed a lot of shit but dont know exactly what so GL, and a new home page ;)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,21 @@ end
 fish_add_path ~/.script
 fish_add_path ~/.local/bin
 
+alias home='cd ~'
+alias root='cd /'
+alias boot='cd /boot'
+alias config='cd ~/.config'
+alias scripts='cd ~/.script'
+
 alias ls='lsd'
 alias cat='bat'
 alias fetch='gxrfetch'
+alias ping="gping"
+alias top="btop"
+
 alias ga='git add .'
 alias gc='git commit -m '
 alias gp='git push'
+
 alias ma='micro *'
 alias m='micro'
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-       printf "%s  %s @ %s     %s %s\n  " (set_color -r) $USER $hostname (prompt_pwd -D 128) (set_color normal)
+    printf "\n%s\n>> " (prompt_pwd -D 128)
 end
